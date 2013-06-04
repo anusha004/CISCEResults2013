c048d212c5a69769ab97791a5d0f08e25f315804
deedydas <dd367@cornell.edu>
2013-06-04 09:10:08 -0700

    Cleaning up remove

--- ICSEResults13/DataBySchool/analyzeSub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-res=`cat *.csv | grep -Eo ','$1',[[:digit:]]{1,3},' | tr -d ',' | sed -E 's/'$1'//g' | sort -n | uniq -c`
-echo "$res" | awk '{print $2}' > tmp
-echo "`seq 0 100`"  | grep -vxF -f tmp | awk '{print 0, $1}' > tmp2
-echo "$res" > tmp3
-sort -nk 2 tmp3 tmp2 
-rm tmp3
-rm tmp2
-rm tmp
