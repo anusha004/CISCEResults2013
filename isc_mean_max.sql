93dc74de4958027969dce5c700402e39686ac74c
Christopher D. Long <galizur@gmail.com>
2013-06-09 23:44:02 -0700

    ISC mean/sd/max for scores

--- isc_mean_max.sql
+++ isc_mean_max.sql
@@ -1,9 +1,11 @@
 select
 subject,
 avg(score::float)::numeric(4,2) as mean,
+stddev_pop(score::float)::numeric(4,2) as sd,
 max(score::integer) as max,
 count(*) as n
 from isc.scores
 where score not in ('XXX','PCA  *','PCNA *','ABS  *','SPCA *','SPCNA*','A','B','C','D','X')
 group by subject
-order by mean asc;
+order by n desc;
+
